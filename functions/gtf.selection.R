0f580698d1cde573cd61b04906bb438875e42869
darinaobukhova <34476147+darinaobukhova@users.noreply.github.com>
2021-11-30 19:32:43 +0100

    Update gtf.selection.R

--- functions/gtf.selection.R
+++ functions/gtf.selection.R
@@ -11,15 +11,7 @@ if (ref_genome=="Hg38"){
 if (ref_genome=="Hg19"){ 
   gtf.whole <- read.table("/ifs/software/inhouse/GSM/mRNA/reference/versions/1.0/Hg19/Hg19_RefSeq_refGene_whole_exonID.gtf",header=TRUE,sep="\t",stringsAsFactors=FALSE); 
  } 
-if (ref_genome=="danRer11"){ 
-  gtf.whole <- read.table("/ifs/software/inhouse/GSM/mRNA/reference/versions/1.0/danRer11/danRer11_RefSeq_whole_exonID.gtf",header=TRUE,sep="\t",stringsAsFactors=FALSE); 
- } 
+  
 if(ref_genome=="mm10"){
   gtf.whole <- read.table("/storage/projects/ngs/analyses/ResearchProjecten/RNAseq/Reference_Sequences/mm10/Mm10_RefSeq_refGene_exonID.gtf",sep="\t",header=TRUE,stringsAsFactors=FALSE);
   }
-
-if(ref_genome=="rn6"){
-  gtf.whole <- read.table("/storage/projects/ngs/analyses/ResearchProjecten/RNAseq/Reference_Sequences/rn6/Rn6_RefSeq_refGene_exonID.gtf",sep="\t",header=TRUE,stringsAsFactors=FALSE);
-  }
-gtf.whole
-}
